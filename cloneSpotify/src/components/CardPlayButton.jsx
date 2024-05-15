2160757134eda8a71cea0c57338a10000463603d
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2024-05-14 22:29:31 -0300

    zustand

--- cloneSpotify/src/components/CardPlayButton.jsx
+++ cloneSpotify/src/components/CardPlayButton.jsx
@@ -1,6 +1,9 @@
 import { Play, Pause} from "./Player"
+import { usePlayerStore } from '../store/playerStore'
 
 export function CardPlayButton ({id}) {
+    const {  } = usePlayerStore(state => state)   
+    
     return (
         <div className="card-play-button rounded-full bg-green-500 p-4">
             <Play/>
